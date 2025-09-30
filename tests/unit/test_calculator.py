9aae5b01b591eddfe603f37af9e2606a06ec092f
pes1ug23am090 <pes1ug23am090@gmail.com>
2025-09-30 11:28:52 +0530

    added and tested with negative values

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -33,4 +33,16 @@ class TestMultiplyDivideWithValidation:
         with pytest.raises(TypeError, match="Division requires numeric inputs"):
             divide("10", 2)
 
+    def test_add_negative_numbers():
+        """Test adding negative numbers"""
+        assert add(-1, -1) == -2
+        assert add(-5, 3) == -2
+
+
+    def test_subtract_negative_numbers():
+        """Test subtracting negative numbers"""
+        assert subtract(-1, -1) == 0
+        assert subtract(-5, -3) == -2
+    
 # TODO: Students will add TestMultiplyDivide class
+
